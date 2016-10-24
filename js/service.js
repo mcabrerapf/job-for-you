68922923e273a5bdbd3560dafa43ac374db9edb0
Alejandro Pavon <misael04@outlook.es>
2016-10-24 14:36:45 +0200

    Services

---
+++ js/service.js
@@ -0,0 +1,24 @@
+angular.module("myApp")
+	.factory('getInfoService', function($http) {
+
+			function getOfferts() {
+				/*return $http.get("https://api.themoviedb.org/3/movie/popular?api_key=12438eee72e6338cb7859c302ec82c09&language=en-US%20Add%20Comment");*/
+			}
+
+			function getCandidate() {
+				return /*$http.get("https://api.themoviedb.org/3/movie/popular?api_key=12438eee72e6338cb7859c302ec82c09&language=en-US%20Add%20Comment");*/
+			}
+			function getRegion(Name) {
+				return $http.get("https://api.infojobs.net/api/1/offer?contractType=indefinido&province=" +Name+ "&category=administracion-empresas")
+
+			function getCompany(category) {
+				return $http.get("https://api.infojobs.net/api/1/offer?contractType=indefinido&province=madrid&category="+category)
+			}
+
+			return {
+				getOfferts: getOfferts,
+				getCandidate: getCandidate,
+				getRegion: getRegion,
+				getCompany: getCompany
+			}
+	})
