8dd8b3c942d3c2333e1fb86415831898993655c4
Alejandro Pavon <misael04@outlook.es>
2016-10-25 15:43:28 +0200

    style

--- js/controllers.js
+++ js/controllers.js
@@ -1,57 +1,44 @@
 angular.module("myApp")
-
-.controller("searchBySalary", function($scope, getInfoService ) {
-	$scope.homeClass = view;
-	$scope.findBestSalaryRegion = function(e) {
-		e.preventDefault();
-		getInfoService.getRegion($scope.regionName)
-		.then( function(response) {
-			$scope.ofertasR = response.data.offers
-			console.log($scope.ofertasR)
-
-		})
-
-	}
-	$scope.findBestSalaryField = function(e) {
-		e.preventDefault();
-		getInfoService.getOffers($scope.fieldName)
-		.then( function(response) {
-			$scope.ofertasF = response.data.offers
-			console.log($scope.ofertasF)
-
-		})
-
-	}
-
-	$scope.findBestSalaryCompany = function(e) {
-		e.preventDefault();
-		getInfoService.getCompany()
-		.then( function(response) {
-			$scope.ofertasC = response.data.offers
-			console.log($scope.ofertasC)
-
-		})
-
-	}
-	$scope.findBestCompanyOffers = function( companyName ) {
-		
-		console.log(companyName);
-
-		getInfoService.getCompanyOffers(companyName)
-		.then( function(response) {
-			$scope.ofertasCC = response.data.offers
-			console.log($scope.ofertasCC)
-
-		})
-
-	}
-
+//Filters by city
+.controller("findBestSalaryRegion", function($scope, getInfoService ) {
+    $scope.findBestSalaryByRegion = function(e) {
+        e.preventDefault();
+        getInfoService.getRegion($scope.regionName)
+        .then( function(response) {
+            $scope.ofertas = response.data.offers
+        })
+    }
+})
+//Filters by work field
+.controller("findBestSalaryField", function($scope, getInfoService ) {
+    $scope.findBestSalaryByField = function(e) {
+        e.preventDefault();
+        getInfoService.getOffers($scope.fieldName)
+        .then( function(response) {
+            $scope.ofertas = response.data.offers
+        })
+    }
+})
+//Shows top paying companies
+.controller("findBestSalaryCompany", function($scope, getInfoService ) {
+    $scope.findBestSalaryByCompany = function(e) {
+        e.preventDefault();
+        getInfoService.getCompany()
+        .then( function(response) {
+            $scope.ofertas = response.data.offers
+        })
+    }
+})
+//Shows all offers by specific company
+.controller("findBestSalaryCompanyOffers", function($scope, getInfoService ) {
+    $scope.findByBestCompanyOffers = function( companyName ) {
+        getInfoService.getCompanyOffers(companyName)
+        .then( function(response) {
+            $scope.ofertasCC = response.data.offers
+            console.log($scope.ofertasCC)
+        })
+    }
 })
 .controller('homeController',function(){
-	$scope.homeClass = home;
+    $scope.homeClass = home;
 })
-
-
-
-
-
